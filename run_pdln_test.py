f4a4adb9ee3d6867dbef5f5d0cbd1ee3e4d61828
Dave Fetterman <fettermania@gmail.com>
2016-03-11 11:13:28 -0800

    Messy pipeline experiment

--- run_pdln_test.py
+++ run_pdln_test.py
@@ -1,5 +1,7 @@
 #pivot_norm.py - main file
 
+# START HERE: http://stackoverflow.com/questions/28124366/can-gridsearchcv-be-used-with-a-custom-classifier
+
 # SECTION : LOAD DATA
 import pandas as pd
 import re
@@ -34,51 +36,77 @@ X_test_array = np.array(X_test)
 y_test_array = np.array(y_test)
 
 # ====== SECTION: Create model =====
-tfidf_ranker = tfidf_pdln.TFIDFRanker(input_docs, slope=.75, relevance_threshold=.05)
-tfidf_ranker.add_normalization_corpus(normalization_corpus)
-print ("DEBUG: Pivot is calculated at ", tfidf_ranker.pivot)
+# # tfidf_ranker = tfidf_pdln.TFIDFRanker(input_docs=input_docs, normalization_corpus=normalization_corpus, slope=.75, relevance_threshold=.05)
+# # tfidf_ranker.add_normalization_corpus(normalization_corpus)
+# print ("DEBUG: Pivot is calculated at ", tfidf_ranker.pivot)
 
-# ===== SECTION: Run test ======
-THRESHOLD_MAX = .4
-THRESHOLD_POINTS = 20
-SLOPE_MAX = 1.5
-SLOPE_POINTS = 30
+# # ===== SECTION: Run test ======
+# THRESHOLD_MAX = .4
+# THRESHOLD_POINTS = 20
+# SLOPE_MAX = 1.5
+# SLOPE_POINTS = 30
 
-def run_test(slope, threshold, X_array, y_array):
-  # predicted = np.empty(len(y_array))
-  tfidf_ranker.set_slope(slope)
-  tfidf_ranker.set_relevance_threshold(threshold)
-  predicted = tfidf_ranker.predict(X_array[:,0], X_array[:,1])    
-  # Fettermania TODO: Is there a different precision/recall definition for IR?
-  # https://en.wikipedia.org/wiki/Precision_and_recall
-  tp = sum(np.logical_and(predicted, y_train_array))
-  fp = sum(np.logical_and(predicted, np.logical_not(y_train_array)))
-  fn = sum(np.logical_and(np.logical_not(predicted), y_train_array))
-  accuracy  = sum(predicted == y_train_array) / len(X_array)
-  precision = 0 if tp + fp == 0 else tp / (tp + fp)
-  recall = 0 if tp + fn == 0 else tp / (tp + fn)
-  fscore = 0 if precision + recall == 0 else 2*(precision * recall)/(precision + recall)
-  print ("(DEBUG: %s) Run test: train = [a: %f, p: %f, r: %f, f: %f], slope=(%f), threshold=(%f)" % (
-    datetime.datetime.now(), accuracy, precision, recall, fscore, slope, threshold))
-  return (accuracy, precision, recall)
+# def run_test(slope, threshold, X_array, y_array):
+#   # predicted = np.empty(len(y_array))
+#   tfidf_ranker.set_slope(slope)
+#   tfidf_ranker.set_relevance_threshold(threshold)
+#   predicted = tfidf_ranker.predict(X_array)    
+#   # Fettermania TODO: Is there a different precision/recall definition for IR?
+#   # https://en.wikipedia.org/wiki/Precision_and_recall
+#   tp = sum(np.logical_and(predicted, y_train_array))
+#   fp = sum(np.logical_and(predicted, np.logical_not(y_train_array)))
+#   fn = sum(np.logical_and(np.logical_not(predicted), y_train_array))
+#   accuracy  = sum(predicted == y_train_array) / len(X_array)
+#   precision = 0 if tp + fp == 0 else tp / (tp + fp)
+#   recall = 0 if tp + fn == 0 else tp / (tp + fn)
+#   fscore = 0 if precision + recall == 0 else 2*(precision * recall)/(precision + recall)
+#   print ("(DEBUG: %s) Run test: train = [a: %f, p: %f, r: %f, f: %f], slope=(%f), threshold=(%f)" % (
+#     datetime.datetime.now(), accuracy, precision, recall, fscore, slope, threshold))
+#   return (accuracy, precision, recall)
 
-accuracy_matrix = np.empty([THRESHOLD_POINTS, SLOPE_POINTS])
-precision_matrix = np.empty([THRESHOLD_POINTS, SLOPE_POINTS])
-recall_matrix = np.empty([THRESHOLD_POINTS, SLOPE_POINTS])
-# TODO: Replace with grid thing or cross-validation
-for threshold_ix in range(THRESHOLD_POINTS):
-  for slope_ix in range(SLOPE_POINTS):
-    threshold = threshold_ix * THRESHOLD_MAX / THRESHOLD_POINTS
-    slope = slope_ix * SLOPE_MAX / SLOPE_POINTS
-    (acc, prec, rec) = run_test(slope, threshold, X_train_array, y_train_array)
-    accuracy_matrix[threshold_ix][slope_ix] = acc
-    precision_matrix[threshold_ix][slope_ix] = prec
-    recall_matrix[threshold_ix][slope_ix] = rec
+# accuracy_matrix = np.empty([THRESHOLD_POINTS, SLOPE_POINTS])
+# precision_matrix = np.empty([THRESHOLD_POINTS, SLOPE_POINTS])
+# recall_matrix = np.empty([THRESHOLD_POINTS, SLOPE_POINTS])
+# # TODO: Replace with grid thing or cross-validation
+# for threshold_ix in range(THRESHOLD_POINTS):
+#   for slope_ix in range(SLOPE_POINTS):
+#     threshold = threshold_ix * THRESHOLD_MAX / THRESHOLD_POINTS
+#     slope = slope_ix * SLOPE_MAX / SLOPE_POINTS
+#     (acc, prec, rec) = run_test(slope, threshold, X_train_array, y_train_array)
+#     accuracy_matrix[threshold_ix][slope_ix] = acc
+#     precision_matrix[threshold_ix][slope_ix] = prec
+#     recall_matrix[threshold_ix][slope_ix] = rec
 
-# ==== SECTION: Show results ======
-plot_result_surface.plot_result_surface("Accuracy", accuracy_matrix, THRESHOLD_MAX, THRESHOLD_POINTS, SLOPE_MAX, SLOPE_POINTS)
-plot_result_surface.plot_result_surface("Precision", precision_matrix, THRESHOLD_MAX, THRESHOLD_POINTS, SLOPE_MAX, SLOPE_POINTS)
-plot_result_surface.plot_result_surface("Recall", recall_matrix, THRESHOLD_MAX, THRESHOLD_POINTS, SLOPE_MAX, SLOPE_POINTS)
+# # ==== SECTION: Show results ======
+# plot_result_surface.plot_result_surface("Accuracy", accuracy_matrix, THRESHOLD_MAX, THRESHOLD_POINTS, SLOPE_MAX, SLOPE_POINTS)
+# plot_result_surface.plot_result_surface("Precision", precision_matrix, THRESHOLD_MAX, THRESHOLD_POINTS, SLOPE_MAX, SLOPE_POINTS)
+# plot_result_surface.plot_result_surface("Recall", recall_matrix, THRESHOLD_MAX, THRESHOLD_POINTS, SLOPE_MAX, SLOPE_POINTS)
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+
+pipe_svc = Pipeline([('pdln', tfidf_pdln.TFIDFRanker(normalization_corpus, input_docs, slope=.99, pivot=.99, relevance_threshold=.99))])
+param_threshold_range = np.linspace(0, .4, 20)
+param_slope_range = np.linspace(0, 1.5, 30)
+print ("GOT INPUT DOCS LENGTH BEFORE START: %d" % (input_docs.shape[0]))
+param_grid = [{'pdln__relevance_threshold': param_threshold_range, \
+#  'pdln__slope': param_slope}]
+  'pdln__slope': param_slope_range, \
+  #'pdln__input_docs': [input_docs], \
+  'pdln__pivot': [37.2]}]
+  #, \
+  #'pdln__normalization_corpus': [normalization_corpus]}] 
+
+print ("=== STARTING GRID SEARCH ===")
+gs = GridSearchCV(estimator=pipe_svc, \
+  param_grid = param_grid, \
+  scoring ='accuracy', \
+  cv = 10, \
+  n_jobs = 1)
+
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+
 
 # ====== SECTION: DEBUG ======
 print ("DEBUG QUERIES")
--- tfidf_pdln.py
+++ tfidf_pdln.py
@@ -10,14 +10,56 @@ import clean_text
 
 class TFIDFRanker(object):
 
-  # Note: Will not have pivot behavior until set_pivot() or add_normalization_corpus() called
-  def __init__(self, input_docs, slope=1, relevance_threshold=.05):
-    self.input_docs = input_docs
-    self.slope = slope
-    self.relevance_threshold = relevance_threshold
-    (self.cv, _tf_array, _df_array, _n_d, self.idf, self.tfidf_unnormalized) = self._generate_tfidf_model_unnormalized(self.input_docs['cleaned_text'])
-    self.tfidf = skpre.normalize(self.tfidf_unnormalized, axis=1)
+  # Fettermania TODO: Why won't these stick to the instance?
+  input_docs = None
+  normalization_corpus = None
 
+  # Note: Will not have pivot behavior until set_pivot() or add_normalization_corpus() called
+  def __init__(self, normalization_corpus=None, input_docs=None, slope=1, pivot=1, relevance_threshold=.05):
+    # print ("INIT CALLED BEGIN, id %d " % (id(self)))
+    # if input_docs is None:
+    #   input_docs = np.array()
+    #   print ("init called with blank input docs")
+    # else:
+    #   print ("INPUT DOCS OK")
+    # if normalization_corpus is None:
+    #   normalization_corpus = pd.DataFrame()
+    #   print ("init called with blank norm corpus")
+    # else:
+    #   print ("NORM CORPUS OK")
+
+    # print ("INIT CALLED WITH SLOPE %d" % (slope)) 
+    if normalization_corpus is not None:   
+      TFIDFRanker.normalization_corpus = normalization_corpus
+
+    if input_docs is not None:   
+      TFIDFRanker.input_docs = input_docs
+
+    print ("NORM CORPUS IS")
+    print (" %d" % (TFIDFRanker.normalization_corpus.shape[0]))
+
+    print ("INPUT DOCS IS ")
+    print ("%d" % (TFIDFRanker.input_docs.shape[0]))
+        
+    self.slope = slope
+    self.pivot = pivot
+    self.relevance_threshold = relevance_threshold
+    
+    (self.cv, _tf_array, _df_array, _n_d, self.idf, self.tfidf_unnormalized) = self._generate_tfidf_model_unnormalized(TFIDFRanker.input_docs['cleaned_text'])
+    self.tfidf = skpre.normalize(self.tfidf_unnormalized, axis=1)
+    
+    self.add_normalization_corpus(TFIDFRanker.normalization_corpus)
+    
+
+    print ("CALLED INIT on object %d" % (id(self)))
+    print ("CALLED INIT WITH s:%f, p:%f, t:%f" % (slope, pivot, relevance_threshold))
+    print ("WITH INPUT DOCS")
+    if input_docs is None:
+      print ("NONE")
+    else: 
+      print (input_docs.shape[0])
+      
+  
   # Note: Particular TF-IDF 
   # TF = term count
   # IDF = log(N/n_j)
@@ -78,7 +120,7 @@ class TFIDFRanker(object):
     indices_top_n = np.argsort(query_scores)[:,::-1][:,:n]
     result = []
     for (array, indices) in zip(query_scores, indices_top_n):
-      df = pd.DataFrame(self.input_docs.iloc[indices])
+      df = pd.DataFrame(TFIDFRanker.input_docs.iloc[indices])
       df['query_score'] = array[indices]
       result.append(df)
     return result
@@ -88,16 +130,49 @@ class TFIDFRanker(object):
     indices_above_threshold = list(map(lambda qs: [i for i,v in enumerate(qs > self.relevance_threshold) if v], query_scores))      
     result = []
     for (array, indices) in zip(query_scores, indices_above_threshold):
-      df = pd.DataFrame(self.input_docs.iloc[indices])
+      df = pd.DataFrame(TFIDFRanker.input_docs.iloc[indices])
       df['query_score'] = array[indices]
       result.append(df)
     return result
 
-  def predict(self, queries, doc_indexes):
+  def predict(self, queries_and_doc_indexes):
+    print ("QUERIES")
+    print (queries_and_doc_indexes)
+    print (queries_and_doc_indexes.shape)
+    print ("GETTING THAT SHAPE")
+    print (queries_and_doc_indexes.iloc[:,0])
+    queries = queries_and_doc_indexes.iloc[:,0]
+    doc_indexes = queries_and_doc_indexes.iloc[:,1]
     doc_results = self.get_documents_over_threshold(queries)  
-    final_results = []
+    final_results = np.empty(len(doc_results))
     # Fettermania: TODD - there has to be a faster way
-    for (doc_index, doc_result) in zip(doc_indexes, doc_results):
-      final_results.append(1 if doc_index in doc_result.index else 0)
+    for i, (doc_index, doc_result) in enumerate(zip(doc_indexes, doc_results)):
+      final_results[i] = (1 if doc_index in doc_result.index else 0)
     return final_results
+
+  # Fettermania TODO this isn't well-defined as 0, 1.  Might be only for classifier
+  def predict_proba(self, query_and_doc_index):
+    result = self.predict(query_and_doc_index)
+    return np.array([0, 1]) if result[0] == 1 else np.array([1, 0])
+
+  def fit(self, X_train, y_train):
+    # Fettermania: fit_params what are those? TODO
+    print ("FIT TODO")
+    return self
+
+  def partial_fit(self, X_train, y_train):
+    print ("PARTIAL FIT TODO")
+    return self
+
+  def get_params(self, deep=True):
+    return {"slope" : self.slope, "relevance_threshold" : self.relevance_threshold, "pivot": self.pivot}
+    #, "normalization_corpus:": TFIDFRanker.normalization_corpus, "input_docs": TFIDFRanker.input_docs}
+
+  def set_params(self, **parameters):
+    for parameter, value in parameters.items():
+      setattr(self, parameter, value)
+      print ("set params called on object %d with" % (id(self)))
+      print (parameter)
+      print (value)
+    return self
 
